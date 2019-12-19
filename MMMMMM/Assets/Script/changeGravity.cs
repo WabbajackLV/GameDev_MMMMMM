10b4b2feca80db79a14afe1f94847c5566b3c970
Wabbajack <bleachlv@inbox.lv>
2019-12-19 14:40:06 +0200

    Added simple gravity change and movement (right,left)
    Need to improve gravity controls (give it a time lock maybe ?)

---
+++ MMMMMM/Assets/Script/changeGravity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeGravity : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update(){
+	if(Input.GetKeyDown(KeyCode.Space)){
+		rb.gravityScale *= -1;
+	}
+	}
+}
