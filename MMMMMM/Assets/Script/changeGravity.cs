50293662f23c10109e378125ea3c31ffc44355bb
Wabbajack <bleachlv@inbox.lv>
2019-12-19 15:42:19 +0200

    Coins
    Added coin animation, coins donw have a script yet

--- MMMMMM/Assets/Script/changeGravity.cs
+++ MMMMMM/Assets/Script/changeGravity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class changeGravity : MonoBehaviour
 {
 	private Rigidbody2D rb;
+	private bool top;
 	
 	void Start(){
 		rb = GetComponent<Rigidbody2D>();
@@ -13,6 +14,16 @@ public class changeGravity : MonoBehaviour
 	void Update(){
 	if(Input.GetKeyDown(KeyCode.Space)){
 		rb.gravityScale *= -1;
+		Rotation();
 	}
+	}
+	
+	void Rotation(){
+		if(top == false){
+			transform.eulerAngles = new Vector3(0, 0, 180f);
+		}else{
+			transform.eulerAngles = Vector3.zero;
+		}
+		top = !top;
 	}
 }
