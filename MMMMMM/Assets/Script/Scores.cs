4f64cd2fb69571f43cec20c56185694c6c299add
Vils123 <karlis996@inbox.lv>
2020-01-26 14:12:57 +0200

    cleaned some things up
    Gameplay side is finished
    
    -still need to fix up the menu
    -maybe a deathsound when hitting a spike (oof) (idk how to do it)
    -too lazy to make a new player sprite, imo the old one is fine

--- MMMMMM/Assets/Script/Scores.cs
+++ MMMMMM/Assets/Script/Scores.cs
@@ -15,12 +15,12 @@ public class Scores : MonoBehaviour {
     void Start () {
         scoreText = GetComponent<Text>();
 		scoreAmount = 0;
-		//totalCoins = GameObject.FindGameObjectsWithTag("Coins").Length + GameObject.FindGameObjectsWithTag("Coin_Hardmode").Length;
+		totalCoins = GameObject.FindGameObjectsWithTag("Coins").Length + GameObject.FindGameObjectsWithTag("Coin_Hardmode").Length;
 		totalCoins = GameObject.FindGameObjectsWithTag("Coins").Length;
 	}
 	
 	void Update() {
-		//scoreAmount = totalCoins - GameObject.FindGameObjectsWithTag("Coins").Length - GameObject.FindGameObjectsWithTag("Coin_Hardmode").Length;
+		scoreAmount = totalCoins - GameObject.FindGameObjectsWithTag("Coins").Length - GameObject.FindGameObjectsWithTag("Coin_Hardmode").Length;
 		scoreAmount = totalCoins - GameObject.FindGameObjectsWithTag("Coins").Length;
 		scoreText.text ="Score: " + scoreAmount;
 		if(scoreAmount >= totalCoins)
